eaf27cb0c0a0c416ae48c8629e29700b4a9e64be
Brian McCarthy <daglamier22@gmail.com>
2024-08-10 12:21:48 -0500

    setup shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
