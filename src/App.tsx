7e77d39c9793c3aec0c67be1b9939b4df28e6d1a
Brian McCarthy <daglamier22@gmail.com>
2024-08-18 08:56:29 -0500

    setup high level root layout and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { ThemeProvider } from "@/components/theme-provider";
+import RootLayout from "@/pages/RootLayout";
+import HomePage from "@/pages/HomePage";
+import AuthPage from "@/pages/AuthPage";
+import ErrorPage from "@/pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "auth", element: <AuthPage /> },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
